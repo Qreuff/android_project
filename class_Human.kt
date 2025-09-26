a1e4e990124df6187712b11b679569bedbf9783d
Qreuff <taburetka1838@gmail.com>
2025-09-26 22:58:48 +0700

    Update class_Human.kt

--- class_Human.kt
+++ class_Human.kt
@@ -4,4 +4,11 @@ open class Human(
     var speed: Double,
     var x: Double = 0.0,
     var y: Double = 0.0
-) : Runnable {}
+) : Runnable {
+
+    var simulationTime: Int = 10   // время симуляции по умолчанию (секунд)
+    var dt: Double = 1.0           // шаг времени (секунда)
+
+    fun setPosition(nx: Double, ny: Double) {
+        x = nx; y = ny
+    }
