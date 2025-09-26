4d3f9feb097dd7364921a3a6648e1a1809b50974
Qreuff <taburetka1838@gmail.com>
2025-09-26 22:58:28 +0700

    Update class_Driver.kt

--- class_Driver.kt
+++ class_Driver.kt
@@ -6,3 +6,10 @@ class Driver(
     y: Double = 0.0,
     val licenseNumber: String
 ) : Human(fullName, age, speed, x, y) {
+
+    override fun move(dt: Double): Pair<Double, Double> {
+        if (dt <= 0.0) return x to y
+        val distance = speed * dt
+        x += distance // прямолинейное движение вдоль оси X
+        return x to y
+    }
