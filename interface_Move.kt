3f526cd33f7a0e4a591fc1a64238724ab4aa4eda
Qreuff <taburetka1838@gmail.com>
2025-09-26 23:09:33 +0700

    Update interface_Move.kt

--- interface_Move.kt
+++ interface_Move.kt
@@ -1,5 +1,6 @@
 interface Movable{
-    open fun move(dt: Double = 1.0): Pair<Double, Double> {
+    interface move_Human{
+        open fun move(dt: Double = 1.0): Pair<Double, Double> {
         if (dt <= 0.0) return x to y
         val theta = Random.nextDouble(0.0, 2 * Math.PI)
         val distance = speed * dt
@@ -10,3 +11,12 @@ interface Movable{
         return x to y
     }
 }
+    interface move_Driver{
+        override fun move(dt: Double): Pair<Double, Double> {
+        if (dt <= 0.0) return x to y
+        val distance = speed * dt
+        x += distance // прямолинейное движение вдоль оси X
+        return x to y
+    }
+}   
+}    
